389773cef06b8bb98bccd8728cd14496bb692455
Ryan Connor <40437041@live.napier.ac.uk>
2023-08-23 19:23:17 +0100

    Adding firestore database functionality

--- WPFAppProject/WPFAppProject/LoginWindow.xaml.cs
+++ WPFAppProject/WPFAppProject/LoginWindow.xaml.cs
@@ -19,6 +19,9 @@ using System.IO;
 using Microsoft.VisualBasic.FileIO;
 using System.Security.Policy;
 using WPFAppProject.Data;
+using Google.Cloud.Firestore;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Google.Rpc.Help.Types;
 
 namespace WPFAppProject
 {
@@ -28,10 +31,19 @@ namespace WPFAppProject
     public partial class LoginWindow : Window
     {
         PasswordHandler passwordHandler = PasswordHandler.getInstance();
+        FirestoreDb db;
 
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoginWindow_Load(object sender, EventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"wpfappdatabaseproject-firebase-adminsdk-7qfur-59c54e3204.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+            db = FirestoreDb.Create("wpfappdatabaseproject");
+            MessageBox.Show("Successful");
         }
 
         //Set window minimum size
@@ -58,13 +70,24 @@ namespace WPFAppProject
             byte[] salt = passwordHandler.GenerateSalt();
             string hashedPassword = passwordHandler.encode(passwordBox.Password, salt);
 
+            /*
             using StreamWriter file = new($"TestPassword.txt");
             file.WriteLine(usernameBox.Text);
             file.WriteLine(Convert.ToBase64String(salt));
             file.WriteLine(hashedPassword);
+            */
+
+            Google.Cloud.Firestore.DocumentReference doc = db.Collection("userLogin").Document(usernameBox.Text);
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"Password", hashedPassword},
+                {"Salt", Convert.ToBase64String(salt)}
+            };
+            doc.SetAsync(data1);
+            MessageBox.Show("Data added");
         }
 
-        private void loginButton_Click(object sender, RoutedEventArgs e)
+        private async void loginButton_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(usernameBox.Text))
             {
@@ -78,13 +101,47 @@ namespace WPFAppProject
                 return;
             }
 
+            /*
             StreamReader sr = new StreamReader($"TestPassword.txt");
             var line = sr.ReadLine();
             line = sr.ReadLine();
             var salt = Convert.FromBase64String(line);
             line = sr.ReadLine();
             var hashedPassword = line;
+            */
 
+            Google.Cloud.Firestore.DocumentReference docref = db.Collection("userLogin").Document(usernameBox.Text);
+            DocumentSnapshot snap = await docref.GetSnapshotAsync();
+
+            var attemptedPassword = "a";
+            var attemptedSalt = "a";
+
+            if (snap.Exists)
+            {
+                Dictionary<string, object> user = snap.ToDictionary();
+                foreach (var item in user){
+                    if (item.Key.Equals("Password"))
+                    {
+                        attemptedPassword = item.Value.ToString();
+                    }
+                    if (item.Key.Equals("Salt"))
+                    {
+                        attemptedSalt = item.Value.ToString();
+                    }
+                }
+            }
+
+            string encoded = passwordHandler.encode(passwordBox.Password, Convert.FromBase64String(attemptedSalt));
+
+            if (encoded.Equals(attemptedPassword))
+            {
+                MessageBox.Show("Correct Password");
+                HomeWindow home = new HomeWindow();
+                home.Show();
+                this.Close();
+            }
+
+            /*
             string encoded = passwordHandler.encode(passwordBox.Password, salt);
 
             if (encoded.Equals(hashedPassword))
@@ -100,6 +157,7 @@ namespace WPFAppProject
             }
 
             sr.Close();
+            */
         }
     }
 }
